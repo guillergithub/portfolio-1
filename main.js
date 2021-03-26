7551fc204a649f67d8fd52c7f46321f2e5c088c7
guillergithub <guillesalazar8@gmail.com>
2021-03-25 22:28:02 -0500

    files deleted

---
+++ main.js
@@ -0,0 +1,22 @@
+/* TYPED.JS */
+
+const typed = new Typed('.type-machine', { 
+    strings: [
+        
+        "<i class='typed-text'></i>",
+        "<i class='typed-text'>I'm Guillermo Salazar.</i>",
+        "<i class='typed-text'>I'm a Web Developer.</i>", 
+        "<i class='typed-text'>I'm a Musician.</i>",
+        "<i class='typed-text'>I'm a Pizza Maker.</i>"
+    ],
+    typeSpeed: 60,
+    startDelay: 1000,
+    backSpeed: 60,
+    backDelay: 1100,
+    loop: true,
+    loopCount: 3,
+    showCursor: true,
+    cursorChar: '|',
+    
+
+})
